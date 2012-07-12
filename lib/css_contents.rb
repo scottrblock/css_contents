cc6b673c24d60d41815c8d27f5aa53e8508917e6
Scott Block <scott.block@hungrymachine.com>
2012-07-12 10:51:46 -0400

    lalal

--- lib/css_contents.rb
+++ lib/css_contents.rb
@@ -1,6 +1,10 @@
 Dir["tasks/**/*.rake"].each { |ext| load ext } if defined?(Rake)
 
-module CssContents
+module CssContents < Rails::Railtie
+    rake_tasks do
+      Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
+    end
+    
     def self.included(base)
       base.extend ClassMethods
     end  
