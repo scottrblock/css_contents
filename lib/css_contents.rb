6684833352f9a4e069072272378937ae59dd3dea
Scott Block <scott.block@hungrymachine.com>
2012-07-12 16:14:17 -0400

    cleaning up commit

--- lib/css_contents.rb
+++ lib/css_contents.rb
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-module CSSContents
-  def self.included(base)
-    base.extend ClassMethods
-  end  
-  
-  module ClassMethods    
-  end
-end
-
-class ActiveRecord::Base
-  include CSSContents
-=======
 module CssContents
     def self.included(base)
       base.extend ClassMethods
@@ -28,5 +15,4 @@ module CssContents
         Dir[File.join(File.dirname(__FILE__),'tasks/*.rake')].each { |f| load f }
       end
   end
->>>>>>> 6754b1f28c13c911576d8c0ce290ff83a70b2a30
 end
